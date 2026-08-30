fa044c2ada74de0bbb2bc16d23b8f6dbd0d1e352
정훈 <hoons1994@naver.com>
2026-08-30 16:31:11 +0900

    fix: Conscrypt 의존성 및 프로바이더 등록으로 exportKeyingMaterial NoSuchMethodException 완벽 해결

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,7 @@ dependencies {
   // Standalone ADB & Wireless Pairing (100% On-Device)
   implementation("com.github.MuntashirAkon:libadb-android:3.1.1")
   implementation("org.bouncycastle:bcpkix-jdk15to18:1.81")
+  implementation("org.conscrypt:conscrypt-android:2.5.2")
 
   // Compose
   implementation(libs.androidx.compose.ui)
