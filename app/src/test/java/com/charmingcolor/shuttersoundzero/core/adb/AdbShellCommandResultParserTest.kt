09d281ed497bd42808496f6b1a7f532aa2e07352
charmingcolor <32384840+hoons1994@users.noreply.github.com>
2026-09-03 21:36:54 +0900

    test: expand adb shell result parser coverage

--- app/src/test/java/com/charmingcolor/shuttersoundzero/core/adb/AdbShellCommandResultParserTest.kt
+++ app/src/test/java/com/charmingcolor/shuttersoundzero/core/adb/AdbShellCommandResultParserTest.kt
@@ -1,6 +1,7 @@
 package com.charmingcolor.shuttersoundzero.core.adb
 
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertNull
 import org.junit.Test
 
@@ -45,5 +46,57 @@ class AdbShellCommandResultParserTest {
 
         assertEquals("prefix $marker:9 suffix", result?.output)
         assertEquals(0, result?.exitCode)
+    }
+
+    @Test
+    fun parseOrNull_whenCommandHasNoOutput_returnsEmptyOutput() {
+        val result = AdbShellCommandResultParser.parseOrNull("$marker:0\n", marker)
+
+        assertNotNull(result)
+        assertEquals("", result?.output)
+        assertEquals(0, result?.exitCode)
+    }
+
+    @Test
+    fun parseOrNull_whenMarkerContainsRegexCharacters_treatsMarkerLiterally() {
+        val specialMarker = "__SSZ.EXIT+[TEST]__"
+        val result = AdbShellCommandResultParser.parseOrNull(
+            "ok\n$specialMarker:7\n",
+            specialMarker
+        )
+
+        assertEquals("ok", result?.output)
+        assertEquals(7, result?.exitCode)
+    }
+
+    @Test
+    fun parseOrNull_whenMultipleCompleteMarkersExist_usesLastMarker() {
+        val result = AdbShellCommandResultParser.parseOrNull(
+            "first\n$marker:1\nsecond\n$marker:0\n",
+            marker
+        )
+
+        assertEquals("first\n$marker:1\nsecond", result?.output)
+        assertEquals(0, result?.exitCode)
+    }
+
+    @Test
+    fun parseOrNull_whenExitCodeIsNotNumeric_returnsNull() {
+        val result = AdbShellCommandResultParser.parseOrNull(
+            "failure\n$marker:not-a-number\n",
+            marker
+        )
+
+        assertNull(result)
+    }
+
+    @Test
+    fun parseOrNull_whenMarkerHasTrailingCharacters_doesNotAcceptIt() {
+        val result = AdbShellCommandResultParser.parseOrNull(
+            "output\n$marker:0 extra\n",
+            marker
+        )
+
+        assertNull(result)
     }
 }
