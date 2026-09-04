db54817a87337dd13f845e77c417994ae0e72e95
charmingcolor <32384840+hoons1994@users.noreply.github.com>
2026-09-05 08:30:41 +0900

    test: keep parser noise cases invalid

--- app/src/test/java/com/charmingcolor/shuttersoundzero/core/adb/AdbShellCommandResultParserTest.kt
+++ app/src/test/java/com/charmingcolor/shuttersoundzero/core/adb/AdbShellCommandResultParserTest.kt
@@ -143,7 +143,7 @@ class AdbShellCommandResultParserTest {
     @Test
     fun parseOrNull_forRandomMarkerLikeNoise_neverTreatsPartialLinesAsCompletion() {
         val random = Random(0xF00DCAFE.toInt())
-        val separators = listOf(" ", "\t", "-", "+", ":", "x")
+        val separators = listOf(" ", "\t", "-", "+", "x")
 
         repeat(512) { index ->
             val prefix = "noise-${random.nextLong()}"
