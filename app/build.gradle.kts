9346aa081f6f4fa6e9041dce1738e5c8bfc0a797
charmingcolor <32384840+hoons1994@users.noreply.github.com>
2026-09-15 18:35:09 +0900

    chore: bump version to 1.5.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.charmingcolor.shuttersoundzero"
         minSdk = 30
         targetSdk = 37
-        versionCode = 153
-        versionName = "1.5.2"
+        versionCode = 154
+        versionName = "1.5.3"
     }
 
     signingConfigs {
