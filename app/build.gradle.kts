65d8a4a0dc27f265565f99143a79cf8b92488968
charmingcolor <32384840+hoons1994@users.noreply.github.com>
2026-09-15 12:01:10 +0900

    chore: v1.5.2 버전 반영

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.charmingcolor.shuttersoundzero"
         minSdk = 30
         targetSdk = 37
-        versionCode = 152
-        versionName = "1.5.1"
+        versionCode = 153
+        versionName = "1.5.2"
     }
 
     signingConfigs {
