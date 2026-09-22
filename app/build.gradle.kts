79491e0971418ca8ebf056ad2768abb73ce85033
정훈 <hoons1994@naver.com>
2026-09-22 11:35:19 +0900

    test: 1.5.7 프리플라이트 버전 준비

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "io.github.hoons1994.shuttersoundzero"
         minSdk = 30
         targetSdk = 37
-        versionCode = 157
-        versionName = "1.5.6"
+        versionCode = 158
+        versionName = "1.5.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
